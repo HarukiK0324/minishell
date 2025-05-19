5909614789ddd9e4ff5585e8555cd1e6639a7032
haruki <71203445+HarukiK0324@users.noreply.github.com>
2025-05-19 17:37:25 +0900

    draft tokenizer

--- tokenizer/tokenizer.c
+++ tokenizer/tokenizer.c
@@ -1,8 +1,8 @@
 #include "minishell.h"
 
-char *ft_strncmp(const char *s1, const char *s2, int n)
+int ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-    while (n && *s1 && *s2 && (*s1 == *s2))
+    while (n > 0 && *s1 && (*s1 == *s2))
     {
         s1++;
         s2++;
@@ -13,16 +13,16 @@ char *ft_strncmp(const char *s1, const char *s2, int n)
     return *(unsigned char *)s1 - *(unsigned char *)s2;
 }
 
-char *ft_strndup(char *string,int n)
+char *ft_strndup(const char *string, size_t n)
 {
-    char *str;
-    int i;
+    char    *str;
+    size_t  i;
 
     str = (char *)malloc(sizeof(char) * (n + 1));
     if (!str)
-        return NULL;
+        return (perror("ft_strndup: malloc failed"),NULL);
     i = 0;
-    while (i < n)
+    while (i < n && string[i])
     {
         str[i] = string[i];
         i++;
@@ -30,6 +30,17 @@ char *ft_strndup(char *string,int n)
     str[i] = '\0';
     return str;
 }
+
+size_t ft_strlen(const char *s)
+{
+    size_t i;
+
+    i = 0;
+    while (s[i])
+        i++;
+    return i;
+}
+
 
 int ft_isspace(char c)
 {
@@ -39,141 +50,161 @@ int ft_isspace(char c)
 int ismetachar(char c)
 {
     return (c == '|' || c == '<' || c == '>' || c == '(' || c == ')' || c == ';' ||
-            c == '&' || c == '$' || c == '`' || c == '\'' || c == '"' || c == '\\' ||
-            c == '=');
+            c == '&' || c == '$' || c == '`' || c == '\\' || c == '=');
 }
 
-int add_word(char *string, Token **token)
+void append_token(Token **token_list, Token *new_token)
 {
-    Token *new_token;
-    int len;
-
-    new_token = (Token *)malloc(sizeof(Token));
     if (!new_token)
         return;
-    len = 0;
-    while(string[len] != '\0' && !ismetachar(string[len]) && !ft_isspace(string[len]))
-        len++;
-    new_token->value = ft_strndup(string, len);
-    new_token->type = TOKEN_WORD;
-    new_token->next = NULL;
-    if(!(*token))
-        *token = new_token;
+    if (!(*token_list))
+    {
+        *token_list = new_token;
+    }
     else
     {
-        Token *temp = *token;
+        Token *temp = *token_list;
         while (temp->next)
             temp = temp->next;
         temp->next = new_token;
     }
+}
+
+int add_word(const char *string, Token **token_list)
+{
+    Token   *new_token;
+    size_t  len;
+
+    len = 0;
+    while (string[len] && !ismetachar(string[len]) && !ft_isspace(string[len]) &&
+           string[len] != '\'' && string[len] != '\"' )
+        len++;
+    new_token = (Token *)malloc(sizeof(Token));
+    if (!new_token)
+        return(perror("add_word: malloc for token failed"),0);
+    new_token->value = ft_strndup(string, len);
+    if (!new_token->value)
+    {
+        free(new_token);
+        return (perror("add_word: ft_strndup failed"),0);
+    }
+    new_token->type = TOKEN_WORD;
+    new_token->next = NULL;
+    append_token(token_list, new_token);
     return len;
 }
 
-char *get_meta_type(char *string)
+TokenType get_meta_type(const char *string)
 {
-    if(ft_strncmp(string, "<<", 2) == 0)
-        return TOKEN_HEREDOC;
-    else if(ft_strncmp(string, ">>", 2) == 0)
-        return TOKEN_APPEND;
-    else if(ft_strncmp(string, "&&", 2) == 0)
-        return TOKEN_AND_IF;
-    else if(ft_strncmp(string, "||", 2) == 0)
-        return TOKEN_OR_IF;
-    else if(ft_strncmp(string, "|", 1) == 0)
-        return TOKEN_PIPE;
-    else if(ft_strncmp(string, "<", 1) == 0)
-        return TOKEN_REDIR_IN;
-    else if(ft_strncmp(string, ">", 1) == 0)
-        return TOKEN_REDIR_OUT;
-    else if(ft_strncmp(string, "(", 1) == 0)
-        return TOKEN_OPEN_PAREN;
-    else if(ft_strncmp(string, ")", 1) == 0)
-        return TOKEN_CLOSE_PAREN;
-    else if(ft_strncmp(string, ";", 1) == 0)
-        return TOKEN_SEMICOLON;
-    else if(ft_strncmp(string, "&", 1) == 0)
-        return TOKEN_AMPERSAND;
-    else if(ft_strncmp(string, "`", 1) == 0)
-        return TOKEN_BACKTICK;
-    else if(ft_strncmp(string, "$", 1) == 0)
-        return TOKEN_DOLLAR;
-    else if(ft_strncmp(string, "\\", 1) == 0)
-        return TOKEN_BACKSLASH;
-    else if(ft_strncmp(string, "=", 1) == 0)
-        return TOKEN_EQUALS;
+    if (ft_strncmp(string, "<<", 2) == 0) return TOKEN_HEREDOC;
+    if (ft_strncmp(string, ">>", 2) == 0) return TOKEN_APPEND;
+    if (ft_strncmp(string, "&&", 2) == 0) return TOKEN_AND_IF;
+    if (ft_strncmp(string, "||", 2) == 0) return TOKEN_OR_IF;
+    if (ft_strncmp(string, "|", 1) == 0) return TOKEN_PIPE;
+    if (ft_strncmp(string, "<", 1) == 0) return TOKEN_REDIR_IN;
+    if (ft_strncmp(string, ">", 1) == 0) return TOKEN_REDIR_OUT;
+    if (ft_strncmp(string, "(", 1) == 0) return TOKEN_OPEN_PAREN;
+    if (ft_strncmp(string, ")", 1) == 0) return TOKEN_CLOSE_PAREN;
+    if (ft_strncmp(string, ";", 1) == 0) return TOKEN_SEMICOLON;
+    if (ft_strncmp(string, "&", 1) == 0) return TOKEN_AMPERSAND;
+    if (ft_strncmp(string, "`", 1) == 0) return TOKEN_BACKTICK;
+    if (ft_strncmp(string, "$", 1) == 0) return TOKEN_DOLLAR;
+    if (ft_strncmp(string, "\\", 1) == 0) return TOKEN_BACKSLASH;
+    if (ft_strncmp(string, "=", 1) == 0) return TOKEN_EQUALS;
+    return TOKEN_WORD;
 }
 
-int add_metachar(char *string, Token **token)
+int add_metachar(const char *string, Token **token_list)
 {
-    Token *new_token;
+    Token       *new_token;
+    TokenType   type;
+    int         len;
 
-    new_token = (Token *)malloc(sizeof(Token));
-    if (!new_token)
-        return;
-    new_token->value = NULL;
-    new_token->type = get_meta_type(new_token->value);
-    new_token->next = NULL;
-    if(!(*token))
-        *token = new_token;
+    type = get_meta_type(string);
+    if (type == TOKEN_HEREDOC || type == TOKEN_APPEND ||
+        type == TOKEN_AND_IF || type == TOKEN_OR_IF)
+        len = 2;
+    else if (type != TOKEN_WORD)
+        len = 1;
     else
-    {
-        Token *temp = *token;
-        while (temp->next)
-            temp = temp->next;
-        temp->next = new_token;
-    }
-    return ft_strlen(new_token->value);
-}
-
-int add_quote(char *string, Token **token,char quote)
-{
-    Token *new_token;
-    int len;
-
+        return 0;
     new_token = (Token *)malloc(sizeof(Token));
     if (!new_token)
-        return;
-    len = 0;
-    string++;
-    while(string[len] != '\0' && string[len] != quote)
-        len++;
+        return (perror("add_metachar: malloc for token failed"),0);
     new_token->value = ft_strndup(string, len);
-    if(quote == '"')
-        new_token->type = TOKEN_DOUBLE_QUOTE;
-    else
-        new_token->type = TOKEN_SINGLE_QUOTE;
-    new_token->next = NULL;
-    if(!(*token))
-        *token = new_token;
-    else
+    if (!new_token->value)
     {
-        Token *temp = *token;
-        while (temp->next)
-            temp = temp->next;
-        temp->next = new_token;
+        free(new_token);
+        return (perror("add_metachar: ft_strndup failed"),0);
     }
+    new_token->type = type;
+    new_token->next = NULL;
+    append_token(token_list, new_token);
+    return len;
+}
+
+int add_quote(const char *string, Token **token_list, char quote_char)
+{
+    Token   *new_token;
+    size_t  len;
+    const char *start_of_content;
+
+    start_of_content = string + 1;
+    len = 0;
+    while (start_of_content[len] != '\0' && start_of_content[len] != quote_char)
+        len++;
+    new_token = (Token *)malloc(sizeof(Token));
+    if (!new_token)
+        return (perror("add_quote: malloc for token failed"),0);
+    new_token->value = ft_strndup(start_of_content, len);
+    if (!new_token->value)
+    {
+        free(new_token);
+        return (perror("add_quote: ft_strndup failed"),0);
+    }
+    new_token->type = (quote_char == '"') ? TOKEN_DOUBLE_QUOTE : TOKEN_SINGLE_QUOTE;
+    new_token->next = NULL;
+    append_token(token_list, new_token);
     return len + 2;
 }
 
-Token *tokenize(char *input)
+Token *tokenize(const char *input)
 {
-    Token *token;
+    Token   *token_head;
+    int     consumed_len;
 
-    token = NULL;
-    while(*input != '\0')
+    token_head = NULL;
+    while (*input)
     {
-        while(*input && ft_isspace((int)*input))
+        while (*input && ft_isspace((unsigned char)*input))
             input++;
-        if(*input == '\0')
+        if (*input == '\0')
             break;
-        else if(*input == '\"')
-            input += add_quote(input, &token, '\"');
-        else if (*input == '\'')
-            input += add_quote(input, &token,'\'');
-        else if(ismetachar(*input))
-            input += add_metachar(input, &token);
+        consumed_len = 0;
+        if (*input == '\'' || *input == '\"')
+            consumed_len = add_quote(input, &token_head, *input);
+        else if (ismetachar(*input))
+            consumed_len = add_metachar(input, &token_head);
         else
-            input += add_word(input, &token);
+            consumed_len = add_word(input, &token_head);
+        if (consumed_len <= 0)
+            return (free_tokens(token_head),NULL);
+        input += consumed_len;
     }
-    return token;
+    return token_head;
+}
+
+void free_tokens(Token *token_list)
+{
+    Token *current;
+    Token *next_token;
+
+    current = token_list;
+    while (current != NULL)
+    {
+        next_token = current->next;
+        free(current->value);
+        free(current);
+        current = next_token;
+    }
 }
