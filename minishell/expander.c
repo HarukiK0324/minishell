709ee6cdb841511559251138eec0d69f9476390f
haruki <71203445+HarukiK0324@users.noreply.github.com>
2025-07-16 09:26:38 +0900

    modified replacement of env var

--- minishell/expander.c
+++ minishell/expander.c
@@ -5,14 +5,128 @@ int ischar(char c)
     return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9') || c == '_');
 }
 
-char *replace_env_var(char *str,int *j)
+char *to_str(int n)
+{
+    char *str;
+    int len;
+    int temp;
+
+    len = 0;
+    temp = n;
+    if (n == 0)
+        return ft_strdup("0");
+    while (temp > 0)
+    {
+        temp /= 10;
+        len++;
+    }
+    str = (char *)malloc(len + 1);
+    if (!str)
+        return (perror("malloc"), NULL);
+    str[len] = '\0';
+    while (n > 0)
+    {
+        str[--len] = (n % 10) + '0';
+        n /= 10;
+    }
+    return str;
+}
+
+char *str_trim(char *str, int *j, int i)
+{
+    char *new_str;
+    int k;
+
+    if(!str || *j < 0 || i < 0 || *j + i >= ft_strlen(str))
+        return (perror("str_trim failed"), NULL);
+    new_str = (char *)malloc(ft_strlen(str) - i);
+    if(!new_str)
+        return (perror("malloc"), NULL);
+    k = 0;
+    while(k < *j)
+    {
+        new_str[k] = str[k];
+        k++;
+    }
+    while(str[k + 1 + i] != '\0')
+    {
+        new_str[k] = str[k + 1 + i];
+        k++;
+    }
+    new_str[k] = '\0';
+    free(str);
+    return new_str;
+}
+
+char *str_insert(char *str, int *j, char *value)
+{
+    char *new_str;
+    int k;
+    int i;
+
+    if(!str || !value)
+        return (perror("str_insert failed"), NULL);
+    new_str = (char *)malloc(ft_strlen(str) + ft_strlen(value) + 1);
+    if(!new_str)
+        return(perror("malloc"), NULL);
+    i = -1;
+    while(++i < *j)
+        new_str[i] = str[i];
+    k = -1;
+    while(value[++k] !='\0')
+        new_str[i + k] = value[k];
+    while(str[i] != '\0')
+    {
+        new_str[i + k] = str[i];
+        i++;
+    }
+    new_str[i + k] = '\0';
+    (*j) += k;
+    free(str);
+    return new_str;
+}
+
+char *replace_env_var(char *str, int *j, int i,char *env_var, t_env *env_list)
+{
+    char *value;
+
+    value = NULL;
+    while(env_list)
+    {
+        if(ft_strcmp(env_var,env_list->key) == 0)
+        {
+            value = env_list->value;
+            break;
+        }
+        env_list = env_list->next;
+    }
+        str = str_trim(str, j, i);
+    if(value && ft_strlen(value))
+        str = str_insert(str,j,value);
+    return str;
+}
+
+char *replace_status(char *str, int *j, int *status)
+{
+    char *status_str;
+
+    status_str = to_str(*status);
+    if (!status_str)
+        return (perror("malloc"), NULL);
+    str = str_trim(str, j, 1);
+    str = str_insert(str, j, status_str);
+    free(status_str);
+    return str;
+}
+
+char *parse_env_var(char *str,int *j,t_env *env_list, int *status)
 {
     int i;
     int k;
     char *env_var;
 
     if(str[*j + 1] == '?')
-        return replace_status(g_signal);
+        return replace_status(str, j, status);
     i = 0;
     while(str[*j + 1 + i] != '\0' && ischar(str[*j + 1 + i]))
         i++;
@@ -21,14 +135,15 @@ char *replace_env_var(char *str,int *j)
         env_var = (char *)malloc(i + 1);
         if (!env_var)
             return (perror("malloc"), NULL);
-        k = 0;
-        while(k < i)
-        {
+        k = -1;
+        while(++k < i)
             env_var[k] = str[*j + 1 + k];
-            k++;
-        }
         env_var[k] = '\0';
+        str = replace_env_var(str, j, i, env_var, env_list);
     }
+    else
+        (*j)++;
+    return str;
 }
 
 char *trim_quote(char *str,int *j,char c)
@@ -59,7 +174,7 @@ char *trim_quote(char *str,int *j,char c)
     return new_str;
 }
 
-char *trim_double_quote(char *str,int *j)
+char *trim_double_quote(char *str,int *j,t_env *env_list, int *status)
 {
     int i;
     char *new_str;
@@ -68,14 +183,14 @@ char *trim_double_quote(char *str,int *j)
     while(str[i] != '"')
     {
         if(str[i] == '$')
-            str = replace_env_var(str,&i);
+            str = parse_env_var(str,&i,env_list, status);
         else
             i++;
     }
     return trim_quote(str,j,'"');
 }
 
-void expand_cmd(t_cmd *cmd)
+void expand_cmd(t_cmd *cmd,t_env *env_list, int *status)
 {
     int i;
     int j;
@@ -89,9 +204,9 @@ void expand_cmd(t_cmd *cmd)
             if(cmd->argv[i][j] == '\'')
                 cmd->argv[i] = trim_quote(cmd->argv[i],&j,'\'');
             else if(cmd->argv[i][j] == '"')
-                cmd->argv[i] = trim_double_quote(cmd->argv[i],&j);
+                cmd->argv[i] = trim_double_quote(cmd->argv[i], &j, env_list, status);
             else if(cmd->argv[i][j] == '$')
-                cmd->argv[i] = replace_env_var(cmd->argv[i],&j);
+                cmd->argv[i] = parse_env_var(cmd->argv[i], &j, env_list, status);
             else
                 j++;
         }
@@ -99,17 +214,17 @@ void expand_cmd(t_cmd *cmd)
     }
 }
 
-int expander(t_node *node)
+void expander(t_node *node,t_env *env_list, int *status)
 {
     if(!node)
-        return NULL;
+        return;
     if(node->type == NODE_CMD)
-        expand_cmd(node->cmd);
+        expand_cmd(node->cmd,env_list, status);
     else
     {
         if(node->lhs)
-            expander(node->lhs);
+            expander(node->lhs, env_list, status);
         if(node->rhs)
-            expander(node->rhs);
+            expander(node->rhs, env_list, status);
     }
 }
