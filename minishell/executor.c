a517a6d816799fabc5eaeeeae967e7114c5fc208
haruki <71203445+HarukiK0324@users.noreply.github.com>
2025-07-16 09:26:54 +0900

    added executor

--- minishell/executor.c
+++ minishell/executor.c
@@ -1,2 +1,20 @@
 #include "minishell.h"
 
+void executor(t_node *ast, t_env *env_list, int *status)
+{
+    if(ast->type == NODE_PIPE)
+        pipe();
+    else if(ast->type == NODE_AND_IF)
+    {
+        executor(ast->lhs, env_list, status);
+        if(*status == 0)
+            executor(ast->rhs, env_list, status);
+    }
+    else if(ast->type == NODE_OR_IF)
+    {
+        executor(ast->lhs, env_list, status);
+        if(*status != 0)
+            executor(ast->rhs, env_list, status);
+    }else if(ast->type == NODE_CMD)
+        exec_cmd();
+}
