7104f23cf9725f0854fbc89a5316687dedd4554e
haruki <71203445+HarukiK0324@users.noreply.github.com>
2025-07-28 03:52:18 +0900

    fixed parser

---
+++ minishell_fixed/executor.c
@@ -0,0 +1,474 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   executor.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hkasamat <hkasamat@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/26 22:40:58 by hkasamat          #+#    #+#             */
+/*   Updated: 2025/07/28 03:49:44 by hkasamat         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	is_builtin(char *cmd)
+{
+	if (ft_strcmp(cmd, "cd") == 0 || ft_strcmp(cmd, "echo") == 0
+		|| ft_strcmp(cmd, "exit") == 0 || ft_strcmp(cmd, "export") == 0
+		|| ft_strcmp(cmd, "pwd") == 0 || ft_strcmp(cmd, "unset") == 0
+		|| ft_strcmp(cmd, "env") == 0)
+		return (1);
+	return (0);
+}
+
+void	exec_builtin(t_env *env_list, t_cmd *cmd, int *status)
+{
+	if (ft_strcmp(cmd->argv->value, "cd") == 0)
+		*status = exec_cd(cmd->argv, env_list);
+	else if (ft_strcmp(cmd->argv->value, "echo") == 0)
+		*status = exec_echo(cmd->argv);
+	else if (ft_strcmp(cmd->argv->value, "exit") == 0)
+		*status = exec_exit(cmd->argv);
+	else if (ft_strcmp(cmd->argv->value, "export") == 0)
+		*status = exec_export(cmd->argv, env_list);
+	else if (ft_strcmp(cmd->argv->value, "pwd") == 0)
+		*status = exec_pwd();
+	else if (ft_strcmp(cmd->argv->value, "unset") == 0)
+		*status = exec_unset(cmd->argv, env_list);
+	else if (ft_strcmp(cmd->argv->value, "env") == 0)
+		*status = exec_env(env_list);
+}
+
+void	free_str_list(char **list)
+{
+	size_t	i;
+
+	if (!list)
+		return ;
+	i = 0;
+	while (list[i])
+		free(list[i++]);
+	free(list);
+}
+
+char	*ft_access(char *path, char *cmd)
+{
+	char	*full_path;
+	int		i;
+	int		j;
+
+	full_path = (char *)malloc(ft_strlen(path) + ft_strlen(cmd) + 2);
+	if (!full_path)
+		return (perror("malloc"), NULL);
+	i = -1;
+	while (path && path[++i] != '\0')
+		full_path[i] = path[i];
+	full_path[i++] = '/';
+	j = 0;
+	while (cmd && cmd[j] != '\0')
+		full_path[i++] = cmd[j++];
+	full_path[i] = '\0';
+	if (access(full_path, F_OK) == -1)
+		return (free(full_path), NULL);
+	return (full_path);
+}
+
+char	*get_path_from_env(char *argv, t_env *env_list)
+{
+	char	**paths;
+	int		i;
+	char	*path;
+
+	i = 0;
+	while (ft_strcmp(env_list->key, "PATH") != 0)
+		env_list = env_list->next;
+	if (!env_list)
+		return (write(STDERR_FILENO, "minishell: ", 11), write(STDERR_FILENO,
+				argv, ft_strlen(argv)), write(STDERR_FILENO,
+				": No such file or directory\n", 28), NULL);
+	paths = ft_split(env_list->value, ':');
+	path = NULL;
+	while (paths && paths[i])
+		path = ft_access(paths[i++], argv);
+	i = 0;
+	free_str_list(paths);
+	if (path)
+		return (path);
+	return (write(STDERR_FILENO, "minishell: ", 11), write(STDERR_FILENO, argv,
+			ft_strlen(argv)), write(STDERR_FILENO, ": command not found\n", 20),
+		NULL);
+}
+
+char	*get_path(char *cmd, t_env *env_list)
+{
+	if (ft_strchar(cmd, '/') == ft_strlen(cmd))
+		return (ft_strdup(cmd));
+	return (get_path_from_env(cmd, env_list));
+}
+
+size_t	ft_token_size(t_token *tokens)
+{
+	size_t	size;
+
+	size = 0;
+	while (tokens)
+	{
+		size++;
+		tokens = tokens->next;
+	}
+	return (size);
+}
+
+size_t	ft_env_size(t_env *env_list)
+{
+	size_t	size;
+
+	size = 0;
+	while (env_list)
+	{
+		size++;
+		env_list = env_list->next;
+	}
+	return (size);
+}
+
+void	free_argv(char **argv, size_t i)
+{
+	size_t	index;
+
+	index = 0;
+	while (index < i)
+	{
+		free(argv[index]);
+		index++;
+	}
+	free(argv);
+}
+
+char	**to_list(t_token *argv)
+{
+	char	**list;
+	size_t	i;
+
+	if (!argv)
+		return (NULL);
+	list = (char **)malloc(sizeof(char *) * (ft_token_size(argv) + 1));
+	if (!list)
+		return (perror("malloc"), NULL);
+	i = 0;
+	while (argv)
+	{
+		list[i] = ft_strdup(argv->value);
+		if (!list[i])
+			return (perror("strdup"), free_argv(list, i), NULL);
+		argv = argv->next;
+		i++;
+	}
+	list[i] = NULL;
+	return (list);
+}
+
+char	*ft_env_join(t_env *env_list)
+{
+	char	*env;
+	int		i;
+	int		j;
+
+	if (!env_list)
+		return (NULL);
+	env = (char *)malloc(ft_strlen(env_list->key) + ft_strlen(env_list->value)
+			+ 2);
+	if (!env)
+		return (perror("malloc"), NULL);
+	i = -1;
+	while (env_list->key[++i] != '\0')
+		env[i] = env_list->key[i];
+	env[i++] = '=';
+	j = 0;
+	while (env_list->value[j] != '\0')
+		env[i++] = env_list->value[j++];
+	env[i] = '\0';
+	return (env);
+}
+
+char	**env_to_environ(t_env *env_list)
+{
+	char	**environ;
+	size_t	i;
+
+	if (!env_list)
+		return (NULL);
+	environ = (char **)malloc(sizeof(char *) * (ft_env_size(env_list) + 1));
+	if (!environ)
+		return (perror("malloc"), NULL);
+	i = 0;
+	while (env_list)
+	{
+		environ[i] = ft_env_join(env_list);
+		if (!environ[i])
+			return (free_argv(environ, i), NULL);
+		env_list = env_list->next;
+		i++;
+	}
+	environ[i] = NULL;
+	return (environ);
+}
+
+void	read_heredoc(t_fd *heredoc_delimiter, int fd)
+{
+	char	*line;
+
+	while (1)
+	{
+		line = readline("> ");
+		if (!line || ft_strcmp(line, heredoc_delimiter->value) == 0)
+			return (free(line));
+		else if (fd != -1)
+		{
+			write(fd, line, ft_strlen(line));
+			write(fd, "\n", 1);
+		}
+		free(line);
+	}
+}
+
+void	parse_heredoc(t_fd *heredoc_delimiter, int fd)
+{
+	char	*line;
+
+	if (!heredoc_delimiter)
+		return ;
+	while (heredoc_delimiter->next)
+	{
+		read_heredoc(heredoc_delimiter, -1);
+		heredoc_delimiter = heredoc_delimiter->next;
+	}
+	read_heredoc(heredoc_delimiter, fd);
+	heredoc_delimiter->fd = fd;
+}
+
+int	ft_heredoc(t_cmd *cmd)
+{
+	int		fd[2];
+	int		wstatus;
+	pid_t	pid;
+
+	if (pipe(fd) == -1)
+		return (perror("pipe"), -1);
+	pid = fork();
+	if (pid < 0)
+		return (perror("fork"), -1);
+	if (pid == 0)
+	{
+		close(fd[0]);
+		parse_heredoc(cmd->heredoc_delimiter, fd[1]);
+		close(fd[1]);
+		exit(EXIT_SUCCESS);
+	}
+	close(fd[1]);
+	waitpid(pid, &wstatus, 0);
+	if (WIFSIGNALED(wstatus))
+		return (-1);
+	return (0);
+}
+
+void	err_msg(char *value, char *msg)
+{
+	write(STDERR_FILENO, "minishell: ", 11);
+	write(STDERR_FILENO, value, ft_strlen(value));
+	write(STDERR_FILENO, msg, ft_strlen(msg));
+}
+
+void	ft_open_fd_in(t_cmd *cmd, t_fd *current)
+{
+	if (current->type == TOKEN_REDIR_IN)
+	{
+		current->fd = open(current->value, O_RDONLY);
+		if (errno == ENOENT)
+			err_msg(current->value, ": No such file or directory\n");
+		else if (errno == EACCES)
+			err_msg(current->value, ": Permission denied\n");
+		else if (current->fd < 0)
+			err_msg(current->value, ": Failed to open file\n");
+	}
+	cmd->fd_in = current->fd;
+}
+
+void	ft_open_fd_out(t_cmd *cmd, t_fd *current)
+{
+	if (current->type == TOKEN_REDIR_OUT)
+	{
+		current->fd = open(current->value, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+		if (errno == ENOENT)
+			err_msg(current->value, ": No such file or directory\n");
+		else if (errno == EACCES)
+			err_msg(current->value, ": Permission denied\n");
+		else if (current->fd < 0)
+			err_msg(current->value, ": Failed to open file\n");
+	}
+	else if (current->type == TOKEN_APPEND)
+	{
+		current->fd = open(current->value, O_WRONLY | O_CREAT | O_APPEND, 0644);
+		if (errno == ENOENT)
+			err_msg(current->value, ": No such file or directory\n");
+		else if (errno == EACCES)
+			err_msg(current->value, ": Permission denied\n");
+		else if (current->fd < 0)
+			err_msg(current->value, ": Failed to open file\n");
+	}
+	cmd->fd_out = current->fd;
+}
+
+int	ft_file_redirection(t_cmd *cmd)
+{
+	t_fd	*current;
+
+	if (!cmd->fds)
+		return (0);
+	current = cmd->fds;
+	while (current)
+	{
+		if (current->type == TOKEN_REDIR_IN || current->type == TOKEN_HEREDOC)
+			ft_open_fd_in(cmd, current);
+		else if (current->type == TOKEN_REDIR_OUT
+			|| current->type == TOKEN_APPEND)
+			ft_open_fd_out(cmd, current);
+		if (cmd->fd_in == -1 || cmd->fd_out == -1)
+			return (-1);
+		current = current->next;
+	}
+	return (0);
+}
+
+void	exec_pipe(t_node *ast, t_env *env_list, int *status)
+{
+	int		fd[2];
+	pid_t	pid;
+
+	if (pipe(fd) == -1)
+	{
+		*status = -1;
+		return (perror("pipe"));
+	}
+	pid = fork();
+	if (pid < 0)
+	{
+		*status = -1;
+		return (perror("fork"));
+	}
+	if (pid == 0)
+	{
+		dup2(fd[1], STDOUT_FILENO);
+		close(fd[0]);
+		close(fd[1]);
+		executor(ast->lhs, env_list, status);
+		exit(*status);
+	}
+	else
+	{
+		dup2(fd[0], STDIN_FILENO);
+		close(fd[0]);
+		close(fd[1]);
+		waitpid(pid, status, 0);
+		if (WIFEXITED(*status))
+			*status = WEXITSTATUS(*status);
+		else if (WIFSIGNALED(*status))
+			*status = 128 + WTERMSIG(*status);
+		executor(ast->rhs, env_list, status);
+		if (WIFEXITED(*status))
+			*status = WEXITSTATUS(*status);
+		else if (WIFSIGNALED(*status))
+			*status = 128 + WTERMSIG(*status);
+	}
+}
+
+void	ft_execve(t_env *env_list, t_cmd *cmd, int *status)
+{
+	char	*path;
+	char	**environ;
+	char	**argv;
+
+	reset_default_signal();
+	if (ft_heredoc(cmd) == -1)
+		exit(EXIT_FAILURE);
+	if (ft_file_redirection(cmd) == -1)
+		exit(EXIT_FAILURE);
+	if (!cmd->argv || !cmd->argv->value)
+		exit(0);
+	path = get_path(cmd->argv->value, env_list);
+	argv = to_list(cmd->argv);
+	environ = env_to_environ(env_list);
+	if (g_status != 0)
+		return (free(path), free_str_list(argv), free_str_list(environ),
+			exit(128 + g_status));
+	if (!path || !argv || !environ)
+		execve(path, argv, environ);
+	if (errno == ENOENT)
+	{
+		*status = 127;
+		return (err_msg(cmd->argv->value, ": command not found\n"));
+	}
+	else if (errno == EACCES)
+	{
+		*status = 126;
+		return (err_msg(cmd->argv->value, ": permission denied\n"));
+	}
+	free(path);
+	free_str_list(argv);
+	free_str_list(environ);
+	err_msg(cmd->argv->value, ": execve failed\n");
+	exit(EXIT_FAILURE);
+}
+
+void	exec_cmd(t_env *env_list, t_cmd *cmd, int *status)
+{
+	pid_t	pid;
+	int		wstatus;
+
+	if (is_builtin(cmd->argv->value))
+		return (exec_builtin(env_list, cmd, status));
+	pid = fork();
+	if (pid < 0)
+	{
+		*status = -1;
+		return (perror("fork"));
+	}
+	if (pid == 0)
+		ft_execve(env_list, cmd, status);
+	else
+	{
+		free_cmd(cmd);
+		if (waitpid(pid, &wstatus, 0) == -1)
+		{
+			*status = 1;
+			return (perror("waitpid"));
+		}
+		if (WIFEXITED(wstatus))
+			*status = WEXITSTATUS(wstatus);
+		else if (WIFSIGNALED(wstatus))
+			*status = 128 + WTERMSIG(wstatus);
+	}
+}
+
+void	executor(t_node *ast, t_env *env_list, int *status)
+{
+    if(!ast)
+		return ;
+	if (ast->type == NODE_PIPE && g_status == 0)
+		exec_pipe(ast, env_list, status);
+	else if (ast->type == NODE_AND_IF && g_status == 0)
+	{
+		executor(ast->lhs, env_list, status);
+		if (*status == 0 && g_status == 0)
+			executor(ast->rhs, env_list, status);
+	}
+	else if (ast->type == NODE_OR_IF && g_status == 0)
+	{
+		executor(ast->lhs, env_list, status);
+		if (*status != 0 && g_status == 0)
+			executor(ast->rhs, env_list, status);
+	}
+	else if (ast->type == NODE_CMD && g_status == 0)
+		exec_cmd(env_list, ast->cmd, status);
+}
---
+++ minishell_fixed/main.c
@@ -0,0 +1,339 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hkasamat <hkasamat@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/26 22:40:11 by hkasamat          #+#    #+#             */
+/*   Updated: 2025/07/28 03:51:12 by hkasamat         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+volatile sig_atomic_t	g_status = 0;
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	if (s == NULL)
+		return (0);
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+int	ft_strcmp(const char *s1, const char *s2)
+{
+	while (*s1 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (*(const unsigned char *)s1 - *(const unsigned char *)s2);
+}
+
+int	check_quote(char *input)
+{
+	while (*input != '\0')
+	{
+		if (*input == '\"')
+		{
+			input++;
+			while (*input != '\0' && *input != '\"')
+				input++;
+			if (*input == '\0')
+				return (-1);
+		}
+		else if (*input == '\'')
+		{
+			input++;
+			while (*input != '\0' && *input != '\'')
+				input++;
+			if (*input == '\0')
+				return (-1);
+		}
+		input++;
+	}
+	return (1);
+}
+
+char	*append(char *s1, char *s2, char c)
+{
+	char	*result;
+	size_t	len1;
+	size_t	len2;
+	int		i;
+	int		j;
+
+	if (!s1 || !s2)
+		return (perror("append failed"), NULL);
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	result = (char *)malloc(len1 + len2 + 2);
+	if (!result)
+		return (perror("malloc failed"), NULL);
+	i = -1;
+	while (s1 && s1[++i] != '\0')
+		result[i] = s1[i];
+	result[i++] = c;
+	j = -1;
+	while (s2 && s2[++j] != '\0')
+		result[i + j] = s2[j];
+	result[i + j] = '\0';
+	free(s1);
+	free(s2);
+	return (result);
+}
+
+void	free_env(t_env *env_list)
+{
+	t_env	*temp;
+
+	while (env_list)
+	{
+		temp = env_list;
+		env_list = env_list->next;
+		free(temp->key);
+		free(temp->value);
+		free(temp);
+	}
+}
+
+size_t	ft_strchar(const char *s, char c)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			return (i);
+		i++;
+	}
+	return (i);
+}
+
+t_env	*init_env(char **environ)
+{
+	t_env	*env_list;
+	t_env	*new_node;
+	int		i;
+
+	env_list = NULL;
+	i = 0;
+	while (environ[i])
+	{
+		new_node = (t_env *)malloc(sizeof(t_env));
+		if (!new_node)
+			return (perror("malloc"), free_env(env_list), NULL);
+		new_node->key = ft_strndup(environ[i], ft_strchar(environ[i], '='));
+		new_node->value = ft_strdup(environ[i] + ft_strchar(environ[i], '=')
+				+ 1);
+		new_node->next = env_list;
+		env_list = new_node;
+		i++;
+	}
+	return (env_list);
+}
+
+void	setup_signal_handlers(void)
+{
+	struct sigaction	sa_int;
+	struct sigaction	sa_quit;
+
+	// Set up SIGINT handler (Ctrl+C)
+	sa_int.sa_handler = handle_sigint;
+	sigemptyset(&sa_int.sa_mask);
+	sa_int.sa_flags = 0;
+	sigaction(SIGINT, &sa_int, NULL);
+	// Ignore SIGQUIT (Ctrl+\)
+	sa_quit.sa_handler = SIG_IGN;
+	sigemptyset(&sa_quit.sa_mask);
+	sa_quit.sa_flags = 0;
+	sigaction(SIGQUIT, &sa_quit, NULL);
+}
+
+void	handle_sigint(int sig)
+{
+	(void)sig;
+	write(STDOUT_FILENO, "\n", 1);
+	rl_on_new_line();
+	// rl_replace_line("", 0);
+	rl_redisplay();
+	g_status = 130;
+	errno = EINTR;
+}
+
+void	reset_default_signal(void)
+{
+	// Reset to default behavior
+	signal(SIGINT, SIG_DFL);
+	signal(SIGQUIT, SIG_DFL);
+}
+
+void	free_ast(t_node *ast)
+{
+	if (!ast)
+		return ;
+	if (ast->cmd)
+		free_cmd(ast->cmd);
+	if (ast->lhs)
+		free_ast(ast->lhs);
+	if (ast->rhs)
+		free_ast(ast->rhs);
+	free(ast);
+}
+
+void print_ast(t_node *ast)
+{
+	if (!ast)
+		return;
+	switch(ast->type)
+	{
+		case NODE_CMD:
+			printf("Command Node:\n");
+			if (ast->cmd)
+			{
+				t_token *arg = ast->cmd->argv;
+				while (arg)
+				{
+					printf("  Arg: %s\n", arg->value);
+					arg = arg->next;
+				}
+				t_fd *fd = ast->cmd->fds;
+				while (fd)
+				{
+					printf("  FD: %s (type: ", fd->value);
+					switch (fd->type)
+					{
+						case TOKEN_REDIR_IN:
+							printf("Redirect In)\n");
+							break;
+						case TOKEN_REDIR_OUT:
+							printf("Redirect Out)\n");
+							break;
+						case TOKEN_HEREDOC:
+							printf("Heredoc)\n");
+							break;
+						case TOKEN_APPEND:
+							printf("Append)\n");
+							break;
+						default:
+							printf("Unknown)\n");
+							break;
+					}
+					fd = fd->next;
+				}
+				t_fd *heredoc = ast->cmd->heredoc_delimiter;
+				while (heredoc)
+				{
+					printf("  Heredoc Delimiter: %s\n", heredoc->value);
+					heredoc = heredoc->next;
+				}
+			}
+			break;
+		case NODE_PIPE:
+			printf("Pipe Node:\n");
+			break;
+		case NODE_AND_IF:
+			printf("And If Node:\n");
+			break;
+		case NODE_OR_IF:
+			printf("Or If Node:\n");
+			break;
+		case NODE_INIT:
+			printf("Init Node:\n");
+			break;
+		default:
+			printf("Unknown Node Type\n");
+			break;
+	}
+	print_ast(ast->lhs);
+	print_ast(ast->rhs);
+}
+
+int	main(int argc, char **argv, char **environ)
+{
+	char	*input;
+	t_token	*tokens;
+	// t_token *saved_tokens;
+	t_node	*ast;
+	t_env	*env_list;
+	int		status;
+
+	setup_signal_handlers(); // Set up signal handlers for Ctrl+C and Ctrl+'\'
+	(void)argc;              // Unused parameter
+	(void)argv;              // Unused parameter
+	env_list = init_env(environ);
+	if (!env_list)
+		return (perror("init_env failed"), 1);
+	// Exit if environment initialization fails
+	while (1)
+	{
+		input = readline("minishell$ ");
+		if (!input)
+			break ; // Exit on EOF (Ctrl+D)
+		if (input && ft_strlen(input) > 0)
+		{
+			while (check_quote(input) == -1)
+				input = append(input, readline("> "), '\n');
+			tokens = tokenize(input);
+			// saved_tokens = tokens;
+			// while(tokens)
+			// {
+			// 	switch(tokens->type)
+			// 	{
+			// 		case TOKEN_WORD:
+			// 			printf("Word:");
+			// 			break;
+			// 		case TOKEN_PIPE:
+			// 			printf("Pipe:");
+			// 			break;
+			// 		case TOKEN_REDIR_IN:
+			// 			printf("Redirect In:");
+			// 			break;
+			// 		case TOKEN_REDIR_OUT:
+			// 			printf("Redirect Out:");
+			// 			break;
+			// 		case TOKEN_HEREDOC:
+			// 			printf("Heredoc:");
+			// 			break;
+			// 		case TOKEN_APPEND:
+			// 			printf("Append:");
+			// 			break;
+			// 		case TOKEN_AND_IF:
+			// 			printf("And If:");
+			// 			break;
+			// 		case TOKEN_OR_IF:
+			// 			printf("Or If:");
+			// 			break;
+			// 		case TOKEN_OPEN_PAREN:	
+			// 			printf("Open Parenthesis:");
+			// 			break;
+			// 		case TOKEN_CLOSE_PAREN:
+			// 			printf("Close Parenthesis:");
+			// 			break;
+			// 		default:
+			// 			printf("Unknown Token:");
+			// 			break;
+			// 	}
+			// 	printf("Token: %s\n", tokens->value);
+			// 	tokens = tokens->next;
+			// }
+			ast = parse(tokens);
+			if (!ast)
+				status = 2;
+			expander(ast, env_list, &status);
+			executor(ast, env_list, &status);
+			free_ast(ast);
+			add_history(input);
+			free(input);
+		}
+	}
+	printf("exit\n");
+	exit(status);
+}
