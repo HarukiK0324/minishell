b222084a6c64156534fc79febf55b1708c188024
hkasamat <hkasamat@student.42.fr>
2025-08-17 11:20:54 +0900

    norm

--- minishell/builtin/builtin8.c
+++ minishell/builtin/builtin8.c
@@ -6,30 +6,30 @@
 /*   By: hkasamat <hkasamat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/17 10:58:06 by hkasamat          #+#    #+#             */
-/*   Updated: 2025/08/17 10:58:57 by hkasamat         ###   ########.fr       */
+/*   Updated: 2025/08/17 11:19:56 by hkasamat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-char *concat_strings(const char *s1, const char *s2)
+char	*concat_strings(const char *s1, const char *s2)
 {
- char *result;
- size_t len1;
- size_t len2;
- size_t i;
+	char	*result;
+	size_t	len1;
+	size_t	len2;
+	size_t	i;
 
- len1 = ft_strlen(s1);
- len2 = ft_strlen(s2);
- result = malloc(len1 + len2 + 1);
- if (!result)
-  return (NULL);
- i = -1;
- while (++i < len1)
-  result[i] = s1[i];
- i = -1;
- while (++i < len2)
-  result[len1 + i] = s2[i];
- result[len1 + len2] = '\0';
- return (result);
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	result = malloc(len1 + len2 + 1);
+	if (!result)
+		return (NULL);
+	i = -1;
+	while (++i < len1)
+		result[i] = s1[i];
+	i = -1;
+	while (++i < len2)
+		result[len1 + i] = s2[i];
+	result[len1 + len2] = '\0';
+	return (result);
 }
